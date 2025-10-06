ef7b21c35477857d241fa4675d5055e3c72e6760
nahuefalconaro <nfalconaro5@gmail.com>
2025-10-06 19:59:10 -0300

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { AppService, Cancion } from './app.service';
 
-@Controller()
+@Controller("canciones")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): Cancion[] {
+    return this.appService.getCanciones();
+  }
+
+  @Post('body')
+  findAll(@Req() request: Request): string {
+    return request.body!.toString();
+  }
+
+  @Get('docs')
+  getDocs(@Query('version') version: String) {
+    return `Documentacion de la API en la version ${version}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `Esta es la cancion ${this.appService.obtenerById(+id)?.nombre}`;
   }
 }
